d221b9b2785a81a163b1b9993eab070ffec6c670
MeetPatel140 <meetsolanki140@gmail.com>
2024-01-09 18:25:25 +0530

    [9 Jan] [Ex 6] Implementing a max elements in array Functionality

---
+++ exercise_6.rb
@@ -0,0 +1,14 @@
+#Write a function to find the maximum element in an array.
+
+def array_max(string)
+  string = string.split(" ")
+  count = 0
+  string.each do
+    count += 1
+ end
+ puts "There are total " + "#{count}" + " elements in your String."
+end
+
+print "Enter any String: "
+input = gets.chomp.to_s
+puts array_max(input)

d8f059ffb586c797202e780a1522a68c057308cb
MeetPatel140 <meetsolanki140@gmail.com>
2024-01-10 12:58:56 +0530

    [10 Jan] [Ex 9] Implementing a left rotate in Array Functionality

--- exercise_9.rb
+++ exercise_9.rb
@@ -3,12 +3,10 @@
 arr = [1,2,3,4,5]
 
 def rotate_array(arr,position)
-  position %= arr.length
-
-  rotated =  arr.drop(position) + arr.take(position)
+  position.times do
+    arr.push(arr.shift)
+  end
+  arr
 end
 
-rotated_array = rotate_array(arr,2)
-
-puts "Original Array: #{arr}"
-puts "Array rotated left by 2 position: #{rotated_array}"
+print rotate_array(arr,2)

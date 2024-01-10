d41e850dd22b8c86a18cb2a75aa5c72b10e1c541
MeetPatel140 <meetsolanki140@gmail.com>
2024-01-10 15:36:02 +0530

    [10 Jan] [Ex 10] Implementing a Array Checking Functionality

---
+++ exercise_10.rb
@@ -0,0 +1,21 @@
+#Implement a function that checks if two arrays are equal (contain the same elements in the same order).
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [1, 2, 3, 4, 5]
+
+def check_array(array1, array2)
+  return false if array1.length != array2.length
+
+  for i in 0...array1.length
+    if array1[i] != array2[i]
+      return false
+    end
+  end
+  true
+end
+
+if check_array(arr1, arr2)
+  puts "Both Arrays are Equal"
+else
+  puts "Both Arrays are not Equal"
+end
